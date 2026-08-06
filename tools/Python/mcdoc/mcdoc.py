ce11e096dd95a6a08b48ae0e8fb8d77b417c9366
Peter Willendrup <pkwi@fysik.dtu.dk>
2026-08-06 18:33:39 +0200

    Fix to instr .tex snippet function

--- tools/Python/mcdoc/mcdoc.py
+++ tools/Python/mcdoc/mcdoc.py
@@ -1332,7 +1332,7 @@ class InstrLatexDocWriter:
         out.append('')
         out.append(r'\section*{Links}')
         out.append(r'\begin{itemize}')
-        out.append(r'  \item \href{run:%s}{Source code} for \texttt{%s}.' % (i.filepath, _tex(os.path.basename(i.filepath))))
+        out.append(r'  \item Instrument source code found in file \texttt{%s}.' % (_tex(os.path.basename(i.filepath))))
         for l in i.links:
             out.append(r'  \item %s' % _tex(l))
         out.append(r'\end{itemize}')
